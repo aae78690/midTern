49b3bb089f409eefb0d95bbce2eac75386ab96c0
aae78690 <isanase78690@gmail.com>
2024-08-22 16:37:57 +0800

    0822主要架構基本完成

--- js/buttom.js
+++ js/buttom.js
@@ -1,2 +1,18 @@
-var buynum = document.getElementsByClassName('toynum');
-console.log(buynum.innertext);
+// 加減按鈕增減數量
+
+// 增加
+function numPlus(){
+    var num = document.getElementById('buyNum');
+    var buynum = parseInt(num.value);
+
+    num.value = buynum +1;
+}
+// 減少
+function numMinus(){
+    var num = document.getElementById('buyNum');
+    var buynum = parseInt(num.value);
+
+    if(buynum >1){
+        num.value = buynum -1;
+    }
+}
