94178e8991ffab83e91e706a423b38a732399513
aae78690 <isanase78690@gmail.com>
2024-08-27 09:57:29 +0800

    0827報告前小修改

--- js/connactbutton.js
+++ js/connactbutton.js
@@ -3,13 +3,14 @@ function submitForm(event) {
     var e = document.getElementById("email").value
     var b = document.getElementById("feedback").value
     // 判斷EMAIL是否符合格式(開頭必須是/^，結尾必須是$/)
-    var ep = /^\w+@+[a-zA-Z0-9]+$/
+    var ep1 = /^\w+@+[a-zA-Z0-9]+$/
+    var ep =/^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z]+$/
 
 
     if (n == "" | b == "" | e == "") {
         event.target.form.default();
     }
-    else if (e.search(ep)!=-1) {
+    else if (e.search(ep)!=-1|e.search(ep1)!=-1) {
         event.preventDefault();
         alert("感謝您的聯繫，我們會盡快給您回覆！")
     }
